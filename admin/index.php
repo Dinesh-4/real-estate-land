1dcc1b541d41a75e859f56d85599f06fcd08c5f5
Dinesh <www.dinesh4224@gmail.com>
2023-11-10 12:06:20 +0000

    some changes

--- admin/index.php
+++ admin/index.php
@@ -35,6 +35,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <title>Moon Admin - Login</title>
+    <style>
+    body {
+        background-image: url('../images/bg-image/admin-bg-login.jpg') !important;
+        background-size: cover;
+    }
+    </style>
 
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
